a3ff25349c61560c3a1a253871b929c79c9cfa77
Luis <luis@greenhost.nl>
2013-09-05 12:08:45 +0200

    Added vpn start-up scripts used for quick testing.

---
+++ scripts/badovpn.bat
@@ -0,0 +1,2 @@
+@"C:\Program Files (x86)\Viper\client\openvpn\openvpn" C:\openvpnd\viper\openvpn\test\badconfig.ovpn
+@echo RETVAL = %ERRORLEVEL%
---
+++ scripts/goodovpn.bat
@@ -0,0 +1,1 @@
+@"C:\Program Files (x86)\Viper\client\openvpn\openvpn" C:\Users\wintest\AppData\Local\Greenhost\Viper\__config-stdout.ovpn
