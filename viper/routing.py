9006aca18bc61f419e120a218550c967c7a33b41
greenhost <luis@greenhost.nl>
2013-11-04 17:16:23 +0100

    added lots of comments in the routing routines and made the state parser a little more robust

--- viper/routing.py
+++ viper/routing.py
@@ -28,12 +28,43 @@
 # either expressed or implied, of the FreeBSD Project.
 #
 """
-Routing Table Tools for windows
-"""
+Routing Table Tools for Windows. These are used to cross-check that
+Internet traffic is correctly routed.
 
+This is how a normal IPv4 routing table looks like
+
+Network Destination        Netmask          Gateway       Interface  Metric
+          0.0.0.0          0.0.0.0    192.168.2.254      192.168.2.1     25
+        127.0.0.0        255.0.0.0         On-link         127.0.0.1    306
+        127.0.0.1  255.255.255.255         On-link         127.0.0.1    306
+        [...]		[...]					[...]			[...]		[...]
+  255.255.255.255  255.255.255.255         On-link       192.168.2.1    281
+
+The routes marked as 'On-link' do not need a gateway because they are local. Windows
+lists the routes in an inverse order to Linux based systems. Linux links the 
+default route last and lists them in the order of prevalence, on Windows they are 
+listed in reverse to their order of prevalence.
+
+When OpenVPN is running in EIP mode and all internet traffic is channeled through it
+a routing trick is added to the table. In addition to your default route:
+
+          0.0.0.0          0.0.0.0    192.168.2.254      192.168.2.1     25
+
+You will see at least two new ones with the same gateway and the same interface:
+
+          0.0.0.0        128.0.0.0     172.xx.xx.xx     172.yy.yy.yy     30
+        128.0.0.0        128.0.0.0     172.xx.xx.xx     172.yy.yy.yy     30
+
+These routes are pushed by the OpenVPN server and basically override your
+default route forcing all Internet traffic to go from your 
+interface (172.yy.yy.yy) through the VPN gateway (172.xx.xx.xx).
+
+If these routes do not exist in your routing table while you are connected to 
+the VPN, you might be leaking traffic through your default route unknowingly.
+"""
+import logging
 from os import popen
 from re import match
-from tools import log
 from pprint import pprint
 
 class InconsistentRoutingTable(Exception):
@@ -41,15 +72,24 @@ class InconsistentRoutingTable(Exception):
 
 def get_default_route():
 	rtr_table = [elem.strip().split() for elem in popen("route print").read().split("Metric\n")[1].split("\n") if match("^[0-9]", elem.strip())]
+	# on windows the default route is always listed first
 	return rtr_table[0]
 
-def get_next_hop():
-	rtr_table = [elem.strip().split() for elem in popen("route print").read().split("Metric\n")[1].split("\n") if match("^[0-9]", elem.strip())]
-	return rtr_table[0][3]
+def get_all_routes():
+	retval = [elem.strip().split() for elem in popen("route print").read().split("Metric\n")[1].split("\n") if match("^[0-9]", elem.strip())]
+	return retval
 
 def get_iface_route(ifaceip):
 	rtr_table = [elem.strip().split() for elem in popen("route print").read().split("Metric\n")[1].split("\n") if ifaceip in elem.strip()]
 	return rtr_table
+
+def get_default_route():
+	routes = get_all_routes()
+	for route in routes:
+		if (route[0] == '0.0.0.0') and (route[1] == '0.0.0.0'): 
+			return route
+
+	return None
 
 def filter_route(destination, netmask, iface):
 	tab = get_iface_route(iface)
@@ -71,9 +111,14 @@ def verify_vpn_routing_table(ifaceip):
 	route2 = filter_route("128.0.0.0", "128.0.0.0", ifaceip)
 
 	if( (len(route1) == 1) and (len(route2) == 1) ):  # if one and only one route was found
+		logging.debug("Verifying routing table for interface '%s': PASSED" % ifaceip)
 		return True
+	# the following condition might not be true, some providers might provide
+	# several of these routes for redundancy's sake. Viper wants to be
+	# conservative about this and wants to make sure that only one EIP gateway exists.
 	elif ( (len(route1) > 1) or (len(route2) > 1) ):
 		raise InconsistentRoutingTable("A routing table with multiple entries for interface %s was found" % ifaceip)
 	else:
+		logging.debug("Verifying routing table for interface '%s': FAILED" % ifaceip)
 		return False
 
