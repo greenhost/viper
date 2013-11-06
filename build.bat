bfa17474963dea5fb572e376a404c2cc09c6e438
greenhost <luis@greenhost.nl>
2013-11-06 19:38:36 +0100

    changed license to GPLv3

--- build.bat
+++ build.bat
@@ -1,29 +1,18 @@
-@rem Copyright (c) 2013 Greenhost VOF and contributors
+@rem Copyright (c) 2013 Greenhost VOF
+@rem https://greenhost.nl -\- https://greenhost.io
 @rem 
-@rem Redistribution and use in source and binary forms, with or without
-@rem modification, are permitted provided that the following conditions are met: 
+@rem This program is free software: you can redistribute it and/or modify
+@rem it under the terms of the GNU Affero General Public License as
+@rem published by the Free Software Foundation, either version 3 of the
+@rem License, or (at your option) any later version.
 @rem 
-@rem 1. Redistributions of source code must retain the above copyright notice, this
-@rem    list of conditions and the following disclaimer. 
-@rem 2. Redistributions in binary form must reproduce the above copyright notice,
-@rem    this list of conditions and the following disclaimer in the documentation
-@rem    and/or other materials provided with the distribution. 
+@rem This program is distributed in the hope that it will be useful,
+@rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+@rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+@rem GNU Affero General Public License for more details.
 @rem 
-@rem THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-@rem ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-@rem WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-@rem DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-@rem ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-@rem (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-@rem LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-@rem ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-@rem (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-@rem SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-@rem 
-@rem The views and conclusions contained in the software and documentation are those
-@rem of the authors and should not be interpreted as representing official policies, 
-@rem either expressed or implied, of the FreeBSD Project.
-@echo off
+@rem You should have received a copy of the GNU Affero General Public License
+@rem along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v3.5
 
