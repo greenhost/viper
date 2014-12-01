15d64c5f5286c9c4cfb891064882ac07fb506261
Luis Fernandez <luis@greenhost.nl>
2014-12-01 15:12:32 +0100

    added a way to test the service only

---
+++ servtest.py
@@ -0,0 +1,20 @@
+
+from viper import routing
+from viper.windows import service
+from viper.openvpn import monitor
+from viper.tools import *
+import traceback
+
+def start():
+	from rpyc.utils.server import ThreadedServer
+    logging.info("OVPN START...")
+	# make sure only connections from localhost are accepted
+	self.svc = ThreadedServer(monitor.RPCService, hostname = 'localhost', port = 18861)
+
+def stop():
+    logging.info("OVPN STOP...")
+    self.svc.close()
+
+if __name__ == '__main__':
+	logging.basicConfig(level=logging.DEBUG)
+	start()
