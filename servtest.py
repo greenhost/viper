68c9e10ff09892c4d78a6176037304cba336c750
Luis Fernandez <luis@greenhost.nl>
2014-12-01 15:26:31 +0100

    no self

--- servtest.py
+++ servtest.py
@@ -1,19 +1,23 @@
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from viper import routing
 from viper.windows import service
 from viper.openvpn import monitor
 from viper.tools import *
 import traceback
 
+SERVICE = None
+
 def start():
+	global SERVICE
 	from rpyc.utils.server import ThreadedServer
-    logging.info("OVPN START...")
+	logging.info("OVPN START...")
 	# make sure only connections from localhost are accepted
-	self.svc = ThreadedServer(monitor.RPCService, hostname = 'localhost', port = 18861)
+	SERVICE = ThreadedServer(monitor.RPCService, hostname = 'localhost', port = 18861)
 
 def stop():
-    logging.info("OVPN STOP...")
-    self.svc.close()
+	logging.info("OVPN STOP...")
+	SERVICE.close()
 
 if __name__ == '__main__':
 	#log_init_service()
